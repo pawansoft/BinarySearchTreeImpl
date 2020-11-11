675c3c720786bd3f88a364f714e558e2efcd6543
Pawan Kumar <pk.soft29@gmail.com>
2020-11-11 18:20:05 +0530

    Refactor : class to add multiple Node

---
+++ main/java/com/BinaryTree/MyBinaryTree.java
@@ -0,0 +1,40 @@
+package com.BinaryTree;
+
+public class MyBinaryTree <K extends Comparable<K>>{
+    
+    private MyBinaryNode<K> root;
+    
+    public void add(K key){
+        this.root = this.addRecursively(root, key);
+    }
+
+    private MyBinaryNode<K> addRecursively(MyBinaryNode<K> currentNode, K key) {
+        if(currentNode == null){
+            return new MyBinaryNode<K>(key);
+        }
+        int compareValue = key.compareTo(currentNode.key);
+
+        if(compareValue == 0){
+            return null;
+        }
+        else if(compareValue < 0){
+            currentNode.setLeft(addRecursively(currentNode.getLeft(), key));
+
+        }else {
+            currentNode.setRight(addRecursively(currentNode.getRight(), key));
+        }
+        return currentNode;
+    }
+    public int bstSize(){
+        return this.getSizeRecursive(root);
+    }
+
+    private int getSizeRecursive(MyBinaryNode<K> currentNode) {
+        if(currentNode == null){
+            return 0;
+        }
+        else {
+            return 1+ this.getSizeRecursive(currentNode.getLeft()) + this.getSizeRecursive(currentNode.right);
+        }
+    }
+}
--- test/java/com/BinaryTree/Test/BinaryTreeTest.java
+++ test/java/com/BinaryTree/Test/BinaryTreeTest.java
@@ -1,6 +1,7 @@
 package com.BinaryTree.Test;
 
 import com.BinaryTree.MyBinaryNode;
+import com.BinaryTree.MyBinaryTree;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -31,5 +32,47 @@ public class BinaryTreeTest {
         boolean isAddedNodeToBST = fNode.getLeft().equals(tNode) &&
                 fNode.getRight().equals(sNode);
         Assert.assertFalse(isAddedNodeToBST);
+    }
+
+    @Test
+    public void ProvideNumbers_WhenAddedInBST_ShouldReturnSize(){
+        MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        binaryTree.add(22);
+        binaryTree.add(40);
+        binaryTree.add(60);
+        binaryTree.add(95);
+        binaryTree.add(11);
+        binaryTree.add(65);
+        binaryTree.add(3);
+        binaryTree.add(16);
+        binaryTree.add(63);
+        binaryTree.add(67);
+
+        int size = binaryTree.bstSize();
+        Assert.assertEquals(13, size);
+    }
+
+    @Test
+    public void ProvideNumbers_WhenNotAddedInBST_TestCaseShouldPass(){
+        MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        binaryTree.add(22);
+        binaryTree.add(40);
+        binaryTree.add(60);
+        binaryTree.add(95);
+        binaryTree.add(11);
+        binaryTree.add(65);
+        binaryTree.add(3);
+        binaryTree.add(16);
+        binaryTree.add(63);
+        binaryTree.add(67);
+
+        int size = binaryTree.bstSize();
+        Assert.assertNotEquals(0, size);
     }
 }
